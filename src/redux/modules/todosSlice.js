feca4cd36dc38143e0ba96d6e2cc0f84eea2b2b0
leolego03 <leolego03@gmail.com>
2023-07-13 10:48:15 +0900

    first commit

---
+++ src/redux/modules/todosSlice.js
@@ -0,0 +1,50 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+/**
+ * 2초 지연을 시키는 함수입니다 (비동기 작업).
+ */
+import { waitTwoSeconds } from '../../utils';
+
+export const __addToDo = createAsyncThunk(
+  '__addToDo',
+  async (payload, thunkAPI) => {
+    await waitTwoSeconds();
+    thunkAPI.dispatch(addTodo(payload));
+    // console.log("Todo add success !!", "payload: ", payload);
+  }
+);
+
+export const __deleteTodo = createAsyncThunk(
+  '__deleteToDo',
+  async (payload, thunkAPI) => {
+    await waitTwoSeconds();
+    thunkAPI.dispatch(deleteTodo(payload));
+    // console.log("Todo delete success !!")
+  }
+);
+
+const initialState = {
+  list: [],
+};
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      return {
+        ...state,
+        list: [...state.list, action.payload],
+      };
+    },
+    deleteTodo: (state, action) => {
+      return {
+        ...state,
+        list: state.list.filter((item) => item.id !== action.payload)
+      }
+    },
+  },
+});
+
+export const { addTodo, deleteTodo } = todosSlice.actions;
+export default todosSlice.reducer;
